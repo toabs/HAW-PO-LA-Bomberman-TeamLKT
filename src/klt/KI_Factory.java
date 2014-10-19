3b9f8a39edca56945fe06a4cf5e1935177219bf1
LarsEE <LE.Engelbrecht@gmx.de>
2014-10-19 18:12:51 +0200

    Added Factory-Class to create KI-instances. Removed KI_Random and KI_Follower

---
+++ src/klt/KI_Factory.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package klt;
+
+/* ************************************************************** */
+/**
+ * @author Lars
+ * 19.10.2014
+ */
+/* *********************************************************** */
+public class KI_Factory
+{
+    /* ************************************************************** */
+    /**
+     * getKI_Random
+     * @param id
+     * @return
+    */ /************************************************************* */
+    public static KI getKI_Random(int id)
+    {
+        try
+        {
+            return new KI(id, new Agent_Random(null), new Environment_Random());
+        } catch (Exception e)
+        {
+            System.out.println("Exception at KI-Creation: " + e.getMessage());
+            return null;
+        }
+    }
+    
+    /* ************************************************************** */
+    /**
+     * getKI_Random
+     * @param id
+     * @return
+    */ /************************************************************* */
+    public static KI getKI_Follower(int id)
+    {
+        try
+        {
+            return new KI(id, new Agent_Follower(null), new Environment_Follower());
+        } catch (Exception e)
+        {
+            System.out.println("Exception at KI-Creation: " + e.getMessage());
+            return null;
+        }
+    }
+}
