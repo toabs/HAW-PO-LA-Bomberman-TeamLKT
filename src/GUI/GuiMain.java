ad8c5d31cca8af0be8ad968949535852962f1637
LarsEE <LE.Engelbrecht@gmx.de>
2014-11-10 09:23:08 +0100

    updated game-Version

--- src/GUI/GuiMain.java
+++ src/GUI/GuiMain.java
@@ -7,20 +7,18 @@ import Core.Game;
 import Core.Human;
 import Core.User;
 
-import klt.KI_Factory;
-
 public class GuiMain {
 	public static void main(String[] args) {
 		
 		List<User> users = new ArrayList<>();		
-		users.add(KI_Factory.getFighterA(1));
-		users.add(KI_Factory.getFighterB(2));
+		users.add(new Human(1));
+		users.add(new Human(2));
 		int boardsize = 15;
 		int bombCounter = 8;
 		int explosionArea = 4;
-		int maxSteps = 300;
-		long gameoverSleep = 0l;
-		long stepSleep = 0l;
+		int maxSteps = 100;
+		long gameoverSleep = 1000l;
+		long stepSleep = 300l;
 		
 		new GuiStart(new Game(users, boardsize, bombCounter, explosionArea, maxSteps, stepSleep), gameoverSleep);
 	}
