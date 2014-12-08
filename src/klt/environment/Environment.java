a74a65e24a03261b7fad1c4c0b8195419046abab
toabs <tobiasheitmann@arcor.de>
2014-12-08 17:15:12 +0100

    rollback to last working version

--- src/klt/environment/Environment.java
+++ src/klt/environment/Environment.java
@@ -33,7 +33,6 @@ public abstract class Environment implements EnvironmentInterface
     protected double maxDistanceToOpponent = 0; //to be calculated
     protected final int numIntegers = 4;
     protected final int numDoubles = 1;
-    protected final double distanceRadiusOffset = 2.0;
     protected final int escapePaths = 256;
     
     //helper variables filled by the functions
@@ -185,13 +184,7 @@ public void setPlayboard(Playboard playboard, int userID)
         int diffx = cP.getX() - oP.getX();
         int diffy = cP.getY() - oP.getY();
     
-        double result = Math.sqrt(Math.pow(diffx, 2) + Math.pow(diffy, 2));
-        
-        if (result < this.board.getExplosionRadius() + distanceRadiusOffset) {
-            return result;
-        } else {
-            return this.board.getExplosionRadius() + distanceRadiusOffset;
-        }
+        return Math.sqrt(Math.pow(diffx, 2) + Math.pow(diffy, 2));
     }
     
     /* ************************************************************** */
